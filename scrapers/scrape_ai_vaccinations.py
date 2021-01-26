11c25cf32ef10c46456cd46f52db713b047ae9c5
Markus Meier <maekke@gentoo.org>
2021-01-26 21:22:32 +0100

    add AI scraper and common helper module

---
+++ scrapers/scrape_ai_vaccinations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import re
+import arrow
+import scrape_common as sc
+
+
+def parse_ai_date(date_str):
+    return arrow.get(date_str, 'DD. MMMM YYYY', locale='de').datetime.date()
+
+
+url = 'https://www.ai.ch/themen/gesundheit-alter-und-soziales/gesundheitsfoerderung-und-praevention/uebertragbare-krankheiten/coronavirus/impfung'
+d = sc.download(url)
+
+vd = sc.VaccinationData(canton='AI', url=url)
+
+res = re.search(r'>Stand\s(.*\s\d{4}),\s\d+\sUhr<', d)
+assert res
+date = res[1]
+date = parse_ai_date(date)
+vd.date = date.isoformat()
+
+res = re.search(r'<li>([0-9]+)\s+Personen geimpft \(kumuliert\)<\/li>', d)
+assert res
+vd.total_vaccinations = res[1]
+
+assert vd
+print(vd)
---
+++ scrapers/scrape_common.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import requests
+
+
+class VaccinationData:
+    __initialized = False
+    SEPARATOR = ','
+
+    def __init__(self, canton=None, url=None):
+        self.date = None
+        self.week = None
+        self.year = None
+        self.canton = canton
+        self.doses_delivered = None
+        self.first_doses = None
+        self.second_doses = None
+        self.total_vaccinations = None
+        self.url = url
+        self.__initialized = True
+
+    def __setattr__(self, key, value):
+        if self.__initialized and not hasattr(self, key):
+            raise TypeError(f'unknown key: {key}')
+        object.__setattr__(self, key, value)
+
+    def __str__(self):
+        res = []
+        res.append(self.canton)
+        res.append('' if self.date is None else str(self.date))
+        res.append('' if self.week is None else str(self.week))
+        res.append('' if self.year is None else str(self.year))
+        res.append('' if self.doses_delivered is None else str(self.doses_delivered))
+        res.append('' if self.first_doses is None else str(self.first_doses))
+        res.append('' if self.second_doses is None else str(self.second_doses))
+        res.append('' if self.total_vaccinations is None else str(self.total_vaccinations))
+        res.append(self.url)
+        return VaccinationData.SEPARATOR.join(res)
+
+    def __bool__(self):
+        attributes = [
+            self.doses_delivered,
+            self.first_doses,
+            self.second_doses,
+            self.total_vaccinations,
+        ]
+        return any(v is not None for v in attributes)
+
+    def parse(self, data):
+        items = data.split(VaccinationData.SEPARATOR)
+        if len(items) == 10:
+            self.canton = items[0]
+            self.date = items[1]
+            self.week = self.__get_int_item(items[2])
+            self.year = self.__get_int_item(items[3])
+            self.doses_delivered = items[4]
+            self.first_doses = items[5]
+            self.second_doses = items[6]
+            self.total_vaccinations = items[7]
+            self.url = items[8]
+            return True
+        return False
+
+    @staticmethod
+    def __get_int_item(item):
+        try:
+            return int(item)
+        except:
+            return None
+
+    @staticmethod
+    def header():
+        return 'canton,date,week,year,doses_delivered,first_doses,second_doses,total_vaccinations,source'
+
+
+def _download(url, encoding='utf-8'):
+    req = requests.get(url)
+    req.raise_for_status()
+    if encoding:
+        req.encoding = encoding
+    return req
+
+
+def download(url, encoding='utf-8'):
+    return _download(url, encoding).text
