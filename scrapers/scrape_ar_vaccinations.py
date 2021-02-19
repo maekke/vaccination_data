46ca0773b0a86dc8b9d0ed6969d89250714dac4f
Markus Meier <maekke@gentoo.org>
2021-02-19 20:54:20 +0100

    remove <strong> tag from page source for AR

--- scrapers/scrape_ar_vaccinations.py
+++ scrapers/scrape_ar_vaccinations.py
@@ -12,6 +12,7 @@ def parse_ar_date(date_str):
 url = 'https://www.ar.ch/verwaltung/departement-gesundheit-und-soziales/amt-fuer-gesundheit/informationsseite-coronavirus/'
 d = sc.download(url)
 d = d.replace('&nbsp;', ' ')
+d = re.sub(r'</?strong>', r'', d)
 d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
 
 vd = sc.VaccinationData(canton='AR', url=url)
@@ -22,11 +23,11 @@ date = res[1]
 date = parse_ar_date(date)
 
 vd.date = date.isoformat()
-res = re.search(r'>Bereits gelieferte Impfdosen: <strong>(\d+)</strong>', d)
+res = re.search(r'>Bereits gelieferte Impfdosen: (\d+)', d)
 assert res
 vd.doses_delivered = res[1]
 
-res = re.search(r'>Bereits verimpfte Impfdosen: <strong>(\d+)</strong>', d)
+res = re.search(r'>Bereits verimpfte Impfdosen: (\d+)', d)
 assert res
 vd.total_vaccinations = res[1]
 print(vd)
