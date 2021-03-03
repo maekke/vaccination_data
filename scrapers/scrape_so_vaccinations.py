0d4ea619b682a4950f6066b24b9bec76ebfd7d61
Markus Meier <maekke@gentoo.org>
2021-03-03 18:50:38 +0100

    update SO scraper to use new website

--- scrapers/scrape_so_vaccinations.py
+++ scrapers/scrape_so_vaccinations.py
@@ -10,30 +10,46 @@ def parse_so_date(date_str):
     return arrow.get(date_str, 'DD.MM.YYYY', locale='de').datetime.date()
 
 
-url = 'https://corona.so.ch/index.php?id=27979'
+url = 'https://corona.so.ch/bevoelkerung/daten/impfstatistik/'
 d = sc.download(url)
+d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
 soup = BeautifulSoup(d, 'html.parser')
 
-table = soup.find('h2', string=re.compile(r'Situation Kanton Solothurn')).find_next('table')
+table = soup.find('h2', string=re.compile(r'Impfstatistik')).find_next('table')
 
 headers = table.find_all('th')
-vaccination_index = None
+date_index = None
+tot_vaccination_index = None
+first_vaccination_index = None
+second_vaccination_index = None
 for i in range(0, len(headers)):
-    if headers[i].text == 'Anzahl Impfungen (Erstimpfung)':
-        vaccination_index = i
+    if headers[i].text.find('Verimpfte Dosen total') >= 0:
+        tot_vaccination_index = i
+    elif headers[i].text.find('Anzahl Dosen erste Impfung') >= 0:
+        first_vaccination_index = i
+    elif headers[i].text.find('Anzahl Dosen zweite Impfung') >= 0:
+        second_vaccination_index = i
+    elif headers[i].text.find('Datum') >= 0:
+        date_index = i
 
-assert vaccination_index, f'Failed to find vaccinations in {headers}'
+assert tot_vaccination_index is not None, f'Failed to find total index in {headers}'
+assert first_vaccination_index is not None, f'Failed to find first index in {headers}'
+assert second_vaccination_index is not None, f'Failed to find second index in {headers}'
+assert date_index is not None, f'Failed to find date index in {headers}'
 
 rows = table.find('tbody').find_all('tr')
 for row in rows:
     tds = row.find_all('td')
-    value = tds[vaccination_index].text
-    res = re.search(r'(\d+)', value)
-    if res:
-        vd = sc.VaccinationData(canton='SO', url=url)
-        vd.first_doses = res[1]
-        vd.total_vaccinations = res[1]
 
-        date = parse_so_date(tds[0].text)
-        vd.date = date.isoformat()
-        print(vd)
+    vd = sc.VaccinationData(canton='SO', url=url)
+
+    date = tds[date_index].text
+    res = re.search(r'(\d+\.\d+\.\d{4}),', date)
+    assert res
+    vd.date = parse_so_date(res[1])
+
+    vd.first_doses = tds[first_vaccination_index].text
+    vd.second_doses = tds[second_vaccination_index].text
+    vd.total_vaccinations = tds[tot_vaccination_index].text
+
+    print(vd)
