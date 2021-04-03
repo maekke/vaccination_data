cac6e1c30885adab907a9abf3219a5299db57e8a
Markus Meier <maekke@gentoo.org>
2021-04-03 16:25:23 +0200

    add date to SG scraper

--- scrapers/scrape_sg_vaccinations.py
+++ scrapers/scrape_sg_vaccinations.py
@@ -6,12 +6,20 @@ from bs4 import BeautifulSoup
 import scrape_common as sc
 
 
+def parse_sg_date(date_str):
+    return arrow.get(date_str, 'DD.MM.YYYY', locale='de').datetime.date()
+
+
 main_url = 'https://www.sg.ch/ueber-den-kanton-st-gallen/statistik/covid-19.html'
 url = 'https://stada.sg.ch/covid/Durchimpfung_SG.html'
 d = sc.download(url)
 soup = BeautifulSoup(d, 'html.parser')
 
 vd = sc.VaccinationData(canton='SG', url=main_url)
+
+res = re.search(r'Aktualisierungsdatum: (\d+\.\d+\.\d{4})', d)
+assert res
+vd.date = parse_sg_date(res[0])
 
 element = soup.find('td', string=re.compile(r'Total verabreichte Impfdosen'))
 element = element.find_next('td')
