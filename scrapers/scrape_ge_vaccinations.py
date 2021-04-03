5309c5a7ce2395d651853073c80c2db58ee56ae3
Markus Meier <maekke@gentoo.org>
2021-04-03 21:09:40 +0200

    simplify GE scraper logic a bit
    extract first/second doses and total vaccinations from text instead of
    the tables.

--- scrapers/scrape_ge_vaccinations.py
+++ scrapers/scrape_ge_vaccinations.py
@@ -8,46 +8,33 @@ import scrape_common as sc
 
 
 def parse_ge_date(date_str):
-    date = arrow.get(date_str, 'DD.MM', locale='de')
+    date = arrow.get(date_str, 'DD MMMM YYYY', locale='fr')
     return datetime.date(year=2021, month=date.month, day=date.day).isoformat()
-
-
-def extract_values(element):
-    res = re.search(r'(\d+)\s?', element.text)
-    assert res
-    doses = int(res[1])
-
-    res = re.search(r'chiffres au (\d+\.\d+)', element.text)
-    assert res
-    date = parse_ge_date(res[1])
-
-    return (date, doses)
 
 
 url = 'https://www.ge.ch/se-faire-vacciner-contre-covid-19/vaccination-chiffres'
 d = sc.download(url)
 d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
+d = d.replace(u'\xa0', u' ')
 soup = BeautifulSoup(d, 'html.parser')
 
-vds = {}
-
-element = soup.find(text=re.compile(r'Cumul des 1')).find_next('td')
-
 vd = sc.VaccinationData(canton='GE', url=url)
-date, doses = extract_values(element)
-vd.date = date
-vd.first_doses = doses
 
-element = soup.find('strong', text=re.compile(r'^\d+$'))
+tot_vacc_re = r'(\d+) doses ont .* au (\d+\s+\w+\s+\d{4})'
+element = soup.find('p', text=re.compile(tot_vacc_re))
 assert element
-vd.total_vaccinations = int(element.text)
-vds[vd.date] = vd
+res = re.search(tot_vacc_re, element.text)
+assert res
+vd.date = parse_ge_date(res[2])
+vd.total_vaccinations = int(res[1])
 
-element = soup.find(text=re.compile(r'Cumul des 2')).find_next('td')
-date, doses = extract_values(element)
-if date == vd.date:
-    vd.second_doses = doses
-    assert vd.first_doses + vd.second_doses == vd.total_vaccinations
+res = re.search(r'.*\s+(\d+)\s+personnes\s+ont\s+re.u\s+la\s+1.*', d)
+assert res
+vd.first_doses = int(res[1])
 
-for key, values in vds.items():
-    print(values)
+res = re.search(r'.*\s+(\d+)\s+personnes\s+ont\s+re.u\s+leur\s+2.*', d)
+assert res
+vd.second_doses = int(res[1])
+
+assert vd
+print(vd)
