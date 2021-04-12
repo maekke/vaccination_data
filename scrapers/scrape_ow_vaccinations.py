16819550ae6bec9505b90f550d494a518e54378c
Markus Meier <maekke@gentoo.org>
2021-04-12 18:06:03 +0200

    update OW scraper

--- scrapers/scrape_ow_vaccinations.py
+++ scrapers/scrape_ow_vaccinations.py
@@ -25,22 +25,22 @@ soup = BeautifulSoup(d, 'html.parser')
 
 vd = sc.VaccinationData(canton='OW', url=url)
 
-date_regex = r'Die Impfstatistik des Kantons Obwalden \(zuletzt aktualisiert: (\d+\.\s+\w+\s+\d{4})\)'
-element = soup.find('em', string=re.compile(date_regex))
+date_regex = r'Impfstatistik Kanton Obwalden \(zuletzt aktualisiert: (\d+\.\s+\w+\s+\d{4})\)'
+element = soup.find('b', string=re.compile(date_regex))
 assert element
 res = re.search(date_regex, element.text)
 assert res
 vd.date = parse_ow_date(res[1])
 
-element = element.find_next('table')
+element = element.find_parent('table')
 
 trs = element.find_all('tr')
-assert len(trs) == 4, f'unexpected number of rows: {trs}'
+assert len(trs) == 5, f'unexpected number of rows: {trs}'
 
-vd.doses_delivered = get_column(trs[0])
-vd.first_doses = get_column(trs[1])
-vd.second_doses = get_column(trs[2])
-vd.total_vaccinations = get_column(trs[3])
+vd.doses_delivered = get_column(trs[1])
+vd.first_doses = get_column(trs[2])
+vd.second_doses = get_column(trs[3])
+vd.total_vaccinations = get_column(trs[4])
 
 assert vd
 print(vd)
