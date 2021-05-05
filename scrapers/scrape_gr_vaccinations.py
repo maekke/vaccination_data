e75ec1a46f2cf1eadfb2bcd6f052bf1ed0706284
Markus Meier <maekke@gentoo.org>
2021-05-05 22:09:09 +0200

    scrape historized data for GR

--- scrapers/scrape_gr_vaccinations.py
+++ scrapers/scrape_gr_vaccinations.py
@@ -11,6 +11,17 @@ def parse_gr_date(date_str):
 
 
 url = 'https://www.gr.ch/DE/institutionen/verwaltung/djsg/ga/coronavirus/info/impfen/Seiten/impfen.aspx'
+
+hist_url = 'https://www.gr.ch/DE/institutionen/verwaltung/djsg/ga/coronavirus/_layouts/15/GenericDataFeed/feed.aspx?PageID=30&ID=g_dbea8372_ed27_48e8_b2c8_b1f7a9643675&FORMAT=JSONRAW'
+d = sc.download_json(hist_url)
+for data in d:
+    vd = sc.VaccinationData(canton='GR', url=url)
+    vd.date = parse_gr_date(data['Stand'])
+    vd.first_doses = int(data['verimpft1'])
+    vd.second_doses = int(data['verimpft2'])
+    vd.total_vaccinations = vd.first_doses + vd.second_doses
+    print(vd)
+
 d = sc.download(url)
 d = re.sub(r'(\d+)\s(\d+)', r'\1\2', d)
 d = re.sub(r'(\d+)&#39;(\d+)', r'\1\2', d)
