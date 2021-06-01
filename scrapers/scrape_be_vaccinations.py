20d3ecf87e440ab0cb0d5d0fd119be4f666f8bdf
Markus Meier <maekke@gentoo.org>
2021-06-01 17:50:36 +0200

    update BE scraper to new website

--- scrapers/scrape_be_vaccinations.py
+++ scrapers/scrape_be_vaccinations.py
@@ -1,42 +1,28 @@
 #!/usr/bin/env python3
 
+import csv
 import re
+from io import StringIO
 import arrow
-from bs4 import BeautifulSoup
 import scrape_common as sc
 
 
 def parse_be_date(date_str):
-    return arrow.get(date_str, 'DD.MM.YY', locale='de').datetime.date()
+    return arrow.get(date_str).datetime.date()
 
 
-url = 'https://www.besondere-lage.sites.be.ch/de/start/impfen.html'
-d = sc.download(url)
-d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
-soup = BeautifulSoup(d, 'html.parser')
+url = 'https://covid-kennzahlen.apps.be.ch/#/de/cockpit'
+csv_url = 'https://raw.githubusercontent.com/openDataBE/VacMeData/develop/vaccination_key_figures.csv'
+d = sc.download(csv_url)
 
-table = soup.find('h2', string=re.compile('Anzahl durchgef.hrte Impfungen im Kanton Bern')).find_next('table')
-tbody = table.find_all('tbody')[0]
-trs = tbody.find_all('tr')
-
-for tr in trs[1:]:
-    tds = tr.find_all('td')
-    assert len(tds) == 3, f'expected 3 rows, but got {len(tds)} ({tds})'
-
+reader = csv.DictReader(StringIO(d), delimiter=',')
+for row in reader:
     vd = sc.VaccinationData(canton='BE', url=url)
-
-    res = re.search(r'(\d+\.\d+\.\d+)', tds[0].text)
-    assert res
-    date = parse_be_date(res[1])
-    vd.date = date.isoformat()
-
-    res = re.search(r'(\d+)\s?', tds[1].text)
-    assert res
-    vd.total_vaccinations = int(res[1])
-
-    res = re.search(r'(\d+)\s?', tds[2].text)
-    assert(res)
-    vd.second_doses = int(res[1])
-    vd.first_doses = vd.total_vaccinations - vd.second_doses
-    assert vd
+    date = row['dateStats']
+    if date == '':
+        continue
+    vd.date = parse_be_date(date)
+    vd.first_doses = int(row['totalSingleVaccinated'])
+    vd.second_doses = int(row['totalDoubleVaccinated'])
+    vd.total_vaccinations = vd.first_doses + vd.second_doses 
     print(vd)
