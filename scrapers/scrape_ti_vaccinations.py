a76c245dda68a89d39ca039a1a94331a979b8818
Markus Meier <maekke@gentoo.org>
2021-06-01 18:00:24 +0200

    update TI scraper to latest data

--- scrapers/scrape_ti_vaccinations.py
+++ scrapers/scrape_ti_vaccinations.py
@@ -12,29 +12,26 @@ def parse_ti_date(date_str):
 
 url = 'https://www4.ti.ch/dss/dsp/covid19/home/'
 d = sc.download(url)
+d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
+d = re.sub(r'(\d+)&#039;(\d+)', r'\1\2', d)
 soup = BeautifulSoup(d, 'html.parser')
 
 vd = sc.VaccinationData(canton='TI', url=url)
 
-element = soup.find('h2', string=re.compile('Vaccinazioni')).find_next('div')
+element = soup.find('h2', string=re.compile('Vaccinazione')).find_next('div')
 res = re.search(r'Stato: (\d+.\d+.\d{2})', element.text)
 assert res
 vd.date = parse_ti_date(res[1])
 
-"""
-element = soup.find('div', string=re.compile(r'Persone con vaccinazione parziale'))
+element = soup.find('div', string='Totale numero dosi somministrate')
 element = element.find_previous('div')
-vd.first_doses = int(element.text)
+vd.total_vaccinations = int(element.text.strip())
 
-element = soup.find('div', string=re.compile(r'Persone con vaccinazione completa'))
+element = soup.find('div', string=re.compile(r'Numero persone con vaccinazione completa\s+\(2 dosi\)'))
 element = element.find_previous('div')
-vd.second_doses = int(element.text)
-vd.first_doses += vd.second_doses
-"""
+vd.second_doses = int(element.text.strip())
 
-element = soup.find('div', string=re.compile(r'Totale numero dosi somministrate'))
-element = element.find_previous('div')
-vd.total_vaccinations = int(element.text)
+vd.first_doses = vd.total_vaccinations - vd.second_doses
 
 assert vd
 print(vd)
