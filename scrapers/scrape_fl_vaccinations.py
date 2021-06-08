5a4a4c29c9528471319bfb49c6e9ddc494dc975c
Markus Meier <maekke@gentoo.org>
2021-06-08 20:37:21 +0200

    update FL scraper to use openpyxl and update to latest xlsx file

--- scrapers/scrape_fl_vaccinations.py
+++ scrapers/scrape_fl_vaccinations.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import datetime
-import xlrd
+import tempfile
+import openpyxl
 import scrape_common as sc
 
 
@@ -9,17 +10,21 @@ url = 'https://www.llv.li/inhalt/118804/amtsstellen/sonderseite-covid-19'
 xls_url = 'https://www.llv.li/files/as/impfungen.xlsx'
 xls_data = sc.download_data(xls_url)
 
-book = xlrd.open_workbook(file_contents=xls_data)
-sheet = book.sheet_by_index(0)
+fp = tempfile.NamedTemporaryFile(suffix='.xlsx')
+fp.write(xls_data)
 
-for i in range(1, 3):
+book = openpyxl.load_workbook(fp.name)
+sheet = book['Impfungen']
+
+for i in range(2, sheet.max_column):
     vd = sc.VaccinationData(canton='FL', url=xls_url)
-    date = sheet.cell_value(3, i)
-    date = datetime.datetime(*xlrd.xldate_as_tuple(date, book.datemode))
-    vd.date = date.date().isoformat()
-    vd.doses_delivered = int(sheet.cell_value(4, i))
-    vd.total_vaccinations = int(sheet.cell_value(5, i))
-    vd.second_doses = int(sheet.cell_value(6, i))
+    date = sheet.cell(4, i)
+    if date.value is None:
+        continue
+    vd.date = date.value.date().isoformat()
+    vd.doses_delivered = int(sheet.cell(5, i).value)
+    vd.total_vaccinations = int(sheet.cell(6, i).value)
+    vd.second_doses = int(sheet.cell(7, i).value)
     vd.first_doses = vd.total_vaccinations - vd.second_doses
 
     assert vd
