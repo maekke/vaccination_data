8e1a5baadd909454225a50dd33a7ad4b7174860d
Markus Meier <maekke@gentoo.org>
2021-07-18 13:21:11 +0200

    migrate AG scraper to openpyxl and update strings/logic

--- scrapers/scrape_ag_vaccinations.py
+++ scrapers/scrape_ag_vaccinations.py
@@ -2,8 +2,9 @@
 
 import datetime
 import re
+import tempfile
 from bs4 import BeautifulSoup
-import xlrd
+import openpyxl
 import scrape_common as sc
 
 
@@ -18,25 +19,27 @@ if not xls_url.startswith('http'):
     xls_url = f'{base_url}{xls_url}'
 xls_data = sc.download_data(xls_url)
 
-book = xlrd.open_workbook(file_contents=xls_data)
-sheet = book.sheet_by_name('6. Impfkampagne')
+fp = tempfile.NamedTemporaryFile(suffix='.xlsx')
+fp.write(xls_data)
+book = openpyxl.load_workbook(fp.name)
+sheet = book['6. Impfkampagne']
 
-title_row = 2
-date_column = 0
+title_row = 3
+date_column = 1
 
 first_dose_column = None
 second_dose_column = None
 total_vaccinations_column = None
 doses_delivered_column = None
-for column in range(title_row, sheet.ncols):
-    value = sheet.cell_value(2, column)
-    if value == 'Total Erstimpfungen (kumuliert)':
+for column in range(2, sheet.max_column):
+    value = sheet.cell(title_row, column).value
+    if re.match(r'(Total)\s+Erstimpfungen\s+\(kumuliert\)', value):
         first_dose_column = column
-    if value == 'Total Zweitimpfungen (kumuliert)':
+    if re.match(r'(Total)\s+Zweitimpfungen\s+\(kumuliert\)', value):
         second_dose_column = column
-    if value == 'Total Impfungen (kumuliert)':
+    if re.match(r'(Total)\s+Impfungen\s+\(kumuliert\)', value):
         total_vaccinations_column = column
-    if value == 'Anzahl gelieferte Impfdosen (kumuliert) (TOTAL)':
+    if re.match(r'(Total)\s+gelieferte\s+Impfdosen\s+\(kumuliert\)', value):
         doses_delivered_column = column
 
 assert first_dose_column
@@ -44,28 +47,18 @@ assert second_dose_column
 assert total_vaccinations_column
 assert doses_delivered_column
 
-for row in range(title_row + 1, sheet.nrows):
-    value = sheet.cell_value(row, first_dose_column)
+for row in range(title_row + 1, sheet.max_row):
+    value = sheet.cell(row, first_dose_column).value
     if value is None or value == '':
         continue
 
     vd = sc.VaccinationData(canton='AG', url=url)
 
-    date = sheet.cell_value(row, date_column)
-    cell_type = sheet.cell_type(row, date_column)
-    if cell_type == 1:
-        vd.week = int(re.search(r'KW(\d+)', date)[1])
-        vd.year = 2021
-        if vd.week == 53:
-            vd.year = 2020
-    else:
-        date = datetime.datetime(*xlrd.xldate_as_tuple(date, book.datemode))
-        vd.date = date.date().isoformat()
-
-    vd.total_vaccinations = int(sheet.cell_value(row, total_vaccinations_column))
-    vd.first_doses = int(sheet.cell_value(row, first_dose_column))
-    vd.second_doses = int(sheet.cell_value(row, second_dose_column))
-    vd.doses_delivered = int(sheet.cell_value(row, doses_delivered_column))
+    vd.date = sheet.cell(row, date_column).value.date().isoformat()
+    vd.total_vaccinations = int(sheet.cell(row, total_vaccinations_column).value)
+    vd.first_doses = int(sheet.cell(row, first_dose_column).value)
+    vd.second_doses = int(sheet.cell(row, second_dose_column).value)
+    vd.doses_delivered = int(sheet.cell(row, doses_delivered_column).value)
 
     assert vd
     print(vd)
