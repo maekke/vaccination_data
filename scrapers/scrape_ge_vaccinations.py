ca6d98a1a26c1ce3bdcea7aaf0d8d405ca1653e7
Markus Meier <maekke@gentoo.org>
2021-12-29 10:39:08 +0100

    fix GE scraper: add another 1k separator and update regex for 1/2 vaccination

--- scrapers/scrape_ge_vaccinations.py
+++ scrapers/scrape_ge_vaccinations.py
@@ -14,6 +14,7 @@ def parse_ge_date(date_str):
 url = 'https://www.ge.ch/se-faire-vacciner-contre-covid-19/chiffres-campagne-vaccination-geneve#chiffres'
 d = sc.download(url)
 d = re.sub(r'(\d+)\'(\d+)', r'\1\2', d)
+d = re.sub(r'(\d+)’(\d+)', r'\1\2', d)
 d = d.replace(u'\xa0', u' ')
 
 vd = sc.VaccinationData(canton='GE', url=url)
@@ -28,11 +29,11 @@ res = re.search(date_re, d)
 assert res
 vd.date = parse_ge_date(res[1])
 
-res = re.search(r'.*\s+(\d+)\s+personnes\s+ont\s+re.u\s+la\s+1.*', d)
+res = re.search(r'<li>(\d+)\s+personnes\s+ont\s+re.u\s+la\s+1', d)
 assert res
 vd.first_doses = int(res[1])
 
-res = re.search(r'.*\s+(\d+)\s+personnes\s+ont\s+re.u\s+leur\s+2.*', d)
+res = re.search(r'<li>(\d+)\s+personnes\s+pour\s+la\s+2', d)
 assert res
 vd.second_doses = int(res[1])
 
