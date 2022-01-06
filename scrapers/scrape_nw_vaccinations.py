414e5612e9d5dcf73aba340218e0feea4130a39e
Markus Meier <maekke@gentoo.org>
2022-01-06 21:08:35 +0100

    approximate first/second doses by fully vaccinated people

--- scrapers/scrape_nw_vaccinations.py
+++ scrapers/scrape_nw_vaccinations.py
@@ -32,11 +32,9 @@ for tr in trs:
     tds = tr.find_all('td')
     assert len(tds) == 2, f'expected 2 items, got: {tds}'
 
-    if re.search('(Bisher zugeteilte Impfdosen)', tds[0].text):
-        vd.doses_delivered = int(tds[1].text)
-
-    if re.search('(\(1. Impfung)', tds[0].text):
+    if re.search('(Vollst.ndig geimpfte Personen)', tds[0].text):
         vd.first_doses = int(tds[1].text)
+        vd.second_doses = int(tds[1].text)
 
     if re.search('(\(2. Impfung)', tds[0].text):
         vd.second_doses = int(tds[1].text)
